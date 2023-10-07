2b28c349e22992ef0a38228f0f0281319b57e8c5
marina <marina.ananyan2003@gmail.com>
2023-10-07 23:57:10 +0600

    2 commit

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import sys
 def add(a, b):
     return a+b
 
+def sub(a, b):
+    return a - b
+
 if __name__ == "__main__":
     a, b = sys.argv[1], sys.argv[2]
+    print(a, b)
+    print(add(a, b))
+    print(sub(a, b))
